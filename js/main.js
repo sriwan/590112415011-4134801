69c5330bd3f6f4242a0307e477699d40d9c3aa08
sriwan <sriwan19012540@gmail.com>
2019-08-18 12:02:36 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function getData(){
+		value = document.getElementById('in1').value;
+
+		showData(value);
+	};
+
+	function showData(msg){
+		console.log(msg);
+		document.getElementById('lb1').innerHTML = msg;
+	};
